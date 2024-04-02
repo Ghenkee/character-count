3a2274c7135670b2525a47c21e14449d1e3cf8f1
JinMenDOG <araki.genki@cloud-ace.jp>
2024-04-02 21:08:01 +0900

    import firebase libraries

--- public/app.js
+++ public/app.js
@@ -1,3 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDxWBV_dFQqNNS9KFCXIWZj8Pk9VPLjGQk',
+  authDomain: 'character-count-jinmendog.firebaseapp.com',
+  projectId: 'character-count-jinmendog',
+  storageBucket: 'character-count-jinmendog.appspot.com',
+  messagingSenderId: '453225436199',
+  appId: '1:453225436199:web:271a4e529b59aef3c2aa02',
+  measurementId: 'G-957FH38XSS',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 function toggleRealTimeCounting() {
   const isChecked = document.getElementById('realTimeCheck').checked;
   document.getElementById('textInput').oninput = isChecked
